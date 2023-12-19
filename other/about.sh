83937e2c4a3c1221c3795a72645238e0d4c5f12c
kyt-team <153147214+kyt-team@users.noreply.github.com>
2023-12-19 21:48:24 +0700

    Add files via upload

---
+++ other/about.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+# Getting
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/kyt-team/izinn/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/Alvi_cell"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282183496832"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
+clear
+echo ""
+echo ""
+echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
+echo -e "                  ${WB}MINI SCRIPT JENGKOLONLINE${NC}"
+echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
+echo -e "  ${WB}»»» Protocol Service «««  |  »»» Network Protocol «««${NC}  "
+echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
+echo -e "  ${YB}- Vless${NC}                   ${WB}|${NC}  ${YB}- Websocket (CDN) non TLS${NC}"
+echo -e "  ${YB}- Vmess${NC}                   ${WB}|${NC}  ${YB}- Websocket (CDN) TLS${NC}"
+echo -e "  ${YB}- Trojan${NC}                  ${WB}|${NC}  ${YB}- gRPC (CDN) TLS${NC}"
+echo -e "  ${YB}- Socks5${NC}                  ${WB}|${NC}"
+echo -e "  ${YB}- Shadowsocks${NC}             ${WB}|${NC}"
+echo -e "  ${YB}- Shadowsocks 2022${NC}        ${WB}|${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "               ${WB}»»» Network Port Service «««${NC}             "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${YB}- HTTPS : 443, 2053, 2083, 2087, 2096, 8443${NC}"
+echo -e "  ${YB}- HTTP  : 80, 8080, 8880, 2052, 2082, 2086, 2095${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
