66a739c4c41cc31fd9f5804ff47e280faa058531
kyt-team <153147214+kyt-team@users.noreply.github.com>
2023-12-19 21:46:48 +0700

    Add files via upload

---
+++ menu/shadowsocks2022.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+# Getting
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/kyt-team/izinn/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/Alvi_cell"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282183496832"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
+clear
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "          ${WB}----- [ Shadowsocks 2022 Menu ] -----${NC}         "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e ""
+echo -e " ${MB}[1]${NC} ${YB}Create Account Shadowsocks 2022${NC} "
+echo -e " ${MB}[2]${NC} ${YB}Trial Account Shadowsocks 2022${NC} "
+echo -e " ${MB}[3]${NC} ${YB}Extend Account Shadowsocks 2022${NC} "
+echo -e " ${MB}[4]${NC} ${YB}Delete Account Shadowsocks 2022${NC} "
+echo -e " ${MB}[5]${NC} ${YB}Check User Login${NC} "
+echo -e ""
+echo -e " ${MB}[0]${NC} ${YB}Back To Menu${NC}"
+echo -e ""
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e ""
+read -p " Select menu :  "  opt
+echo -e ""
+case $opt in
+1) clear ; add-ss2022 ; exit ;;
+2) clear ; trialss2022 ; exit ;;
+3) clear ; extend-ss2022 ; exit ;;
+4) clear ; del-ss2022 ; exit ;;
+5) clear ; cek-ss2022 ; exit ;;
+0) clear ; menu ; exit ;;
+x) exit ;;
+*) echo -e "salah tekan " ; sleep 1 ; shadowsocks2022 ;;
+esac
